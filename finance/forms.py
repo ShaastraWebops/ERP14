eed762082831be7faee6d026346a7207dd6ad91c
Samir Otiv <samirotiv@fakeserver.com>
2013-07-29 18:28:25 +0530

    Created forms for Finance models

---
+++ finance/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.forms import ModelForm
+from finance.models import PaymentRequest, AdvanceRequest, VoucherRequest
+
+
+class VoucherForm(ModelForm):
+    class Meta:
+        model = VoucherRequest
+        fields = ['amount', 'purpose', 'vendor']
+
+
+class PaymentForm(ModelForm):
+    class Meta:
+        model = PaymentRequest
+        fields = ['amount', 'purpose']
+        
+
+class AdvanceForm(ModelForm):
+    class Meta:
+        model = AdvanceRequest
+        fields = ['amount', 'purpose', 'infavorof']
+    
