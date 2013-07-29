212e88d1ea22d3dd1a328fec5cf47bb6f146f3f0
Samir Otiv <samirotiv@fakeserver.com>
2013-07-29 22:48:36 +0530

    Added models to Finance admin

--- finance/admin.py
+++ finance/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from finance.models import Vendor, VoucherRequest, AdvanceRequest, FinUniqueID
+from finance.models import Vendor, VoucherRequest, AdvanceRequest, FinUniqueID, PaymentRequest
 
 admin.site.register(Vendor)
 admin.site.register(VoucherRequest)
 admin.site.register(AdvanceRequest)
+admin.site.register(PaymentRequest)
 admin.site.register(FinUniqueID)
