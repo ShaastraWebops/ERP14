77e36b16440a06a99275a15b5d6dc33d97ddc103
Samir Otiv <samirotiv@fakeserver.com>
2013-09-29 17:26:06 +0530

    Updated URLs

--- facilities/urls.py
+++ facilities/urls.py
@@ -5,11 +5,12 @@ urlpatterns = patterns('facilities.views',
     #Facilities Home
     url(r'^home/$', 'FacilitiesHome'),
     
-    #Page to create an order
-    url(r'^create/$', 'CreateOrder'),
-    
-    #View to clear an order 
+    #Order Management Views
+    url(r'^create/$', 'CreateOrder'), 
     url(r'^approve/(?P<primkey>\d+)/$', 'ApproveOrder'),
+    url(r'^delentry/(?P<primkey>\d+)/$', 'DeleteEntry'),
+    url(r'^editentry/(?P<primkey>\d+)/$', 'EditEntry'),
+    url(r'^delorder/(?P<primkey>\d+)/$', 'DeleteOrder'),
     
     #Add Item to Catalog
     url(r'^addmenuitem/$', 'AddMenuItem'),
