bbb6f415b477ec3517289c9c7c715d79e71aea00
Samir Otiv <samirotiv@fakeserver.com>
2013-10-15 19:30:50 +0530

    Updated URLs

--- finance/urls.py
+++ finance/urls.py
@@ -7,7 +7,9 @@ urlpatterns = patterns('finance.views',
     url(r'^vouchers/$', 'vouchers'),
     
     ##BUDGETING
+    url(r'^budgeting/$', 'finance_budget_portal'),
     url(r'^createbudget/$', 'create_budget'),
     url(r'^approvebudget/(?P<primkey>\d+)/(?P<option>\d+)/$', 'approve_budget'),
+    url(r'^viewbudget/(?P<primkey>\d+)/$', 'budget_page'),
                                                                                    
 )
