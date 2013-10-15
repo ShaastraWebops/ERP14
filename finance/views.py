eba0563de00b35bb7cf96bb991e0e20496b17122
Samir Otiv <samirotiv@fakeserver.com>
2013-10-15 18:21:06 +0530

    Budget Approval & Creation views

--- finance/views.py
+++ finance/views.py
@@ -1,8 +1,63 @@
-from django.shortcuts import render_to_response, render, get_object_or_404
+from django.shortcuts import render_to_response, render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from misc.dajaxice.core import dajaxice_functions
-from finance.models import Vendor
+from finance.models import Vendor, BudgetProposal
+import datetime
+
+#Importing Model Formset Factory
+from django.forms.models import modelform_factory
+
+#Import Django Exception
+from django.core.exceptions import ObjectDoesNotExist
+
+##FOR TESTING
+from django.http import HttpResponse
+
+
+@login_required
+def create_budget(request):
+    BudgetForm = modelform_factory(BudgetProposal, fields=('comment', 'plan_1_total', 'plan_2_total', 'plan_1_description', 'plan_2_description') )
+    
+    if request.method == 'POST':
+        form = BudgetForm(request.POST)
+        if form.is_valid():
+            newbudget = form.save(commit=False)
+            
+            newbudget.creator = request.user.get_profile()
+            newbudget.event = request.user.get_profile().event
+            
+            newbudget.save()
+            
+            return redirect('finance.views.home')
+        else:
+            return render_to_response ('finance/createbudget.htm', {'form': form }, context_instance=RequestContext(request))
+    else:
+        #Display Blank Form
+        form = BudgetForm()
+        context = {'form': form}
+        return render_to_response('finance/createbudget.htm', context, context_instance=RequestContext(request))
+        
+
+@login_required
+def approve_budget(request, primkey, option):
+    try:
+        approvedbudget = BudgetProposal.objects.get(pk=primkey)
+        
+        #Validate
+        if (approvedbudget.isapproved == True) or (int(option) > 2) or (int(option) < 1):
+            return HttpResponse("ERROR")
+        
+        approvedbudget.isapproved = True
+        approvedbudget.dateapproved = datetime.datetime.now()
+        approvedbudget.approver = request.user.get_profile()
+        approvedbudget.selectedplan = option
+        approvedbudget.save()
+        
+        return HttpResponse("Peace.")
+    except ObjectDoesNotExist:
+        return HttpResponse("No object.")
+
 
 
 @login_required
