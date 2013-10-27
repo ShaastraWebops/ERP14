bb26a40a13950161cacdef73cb6910d4f2f7aad5
Akshay Seshadri <akshayseshadri@gmail.com>
2013-10-28 02:15:13 +0530

    Spons file upload bootstrapped

--- files_upload/models.py
+++ files_upload/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class files_model(models.Model):
-    title=models.CharField(max_length=100,help_text='(add a title to your image)')
-    sponsor=models.CharField(max_length=100,choices=(('gen spons','gen spons'),('events spons',(('fr','fr'),('robo','robo')))))
-    attachment=models.ImageField(upload_to='varun/',help_text='(please keep file size below 5MB and give only imagess in jpg format)')
+    title=models.CharField(max_length=100,help_text='(Add a title to your image)')
+    sponsor=models.CharField(max_length=100,choices=(('gen spons','Gen Spons'),('Events Spons',(('fr','FR'),('robo','Robo')))))
+    attachment=models.ImageField(upload_to='varun/',help_text='(Please keep file size below 5MB and give only images in jpg format)')
     #user=models.OneToOneField(User)uncomment this after implementing login
--- files_upload/views.py
+++ files_upload/views.py
@@ -16,14 +16,14 @@ def upload_file(request):
             title=request.POST['title']
             check=files_model.objects.filter(title=title)#uncomment this after implementing login,user=user)
             if check:
-                error='please select another title for your file,it has already been used'
+                error='Please select another title for your file, it has already been used'
             else:
                 form.save()
                 #files will be uploaded inside media folder(MEDIA_ROOT/varun)
-                message='your upload has been successful,please select a file to upload again'
+                message='Your upload has been successful, please select a file to upload again'
     else:
         form = UploadFileForm()
-        message='select a file to upload'
+        message='Select a file to upload'
     return render_to_response('files_upload/upload.html', locals(), context_instance=RequestContext(request))
 
 
