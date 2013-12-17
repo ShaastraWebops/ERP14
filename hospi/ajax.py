0224cb7f260d59652de3404543ed6427ac66cddc
SourbhBh <eccentricph@gmail.com>
2013-12-17 19:14:13 +0530

    RoomMap done

--- hospi/ajax.py
+++ hospi/ajax.py
@@ -13,20 +13,11 @@ from hospi.models import *
 import json 
 
 @dajaxice_register
-def roommap(request):  
+def roommap(request,hostel_name):  
+    
     dajax = Dajax()
-    alak = AvailableRooms.objects.filter(hostel='Alakananda').order_by('room_no')
-    ganga = AvailableRooms.objects.filter(hostel='Ganga').order_by('room_no')
-    jam = AvailableRooms.objects.filter(hostel='Jamuna').order_by('room_no')
-    mahanadhi = AvailableRooms.objects.filter(hostel='Mahanadhi').order_by('room_no')
-    mandak = AvailableRooms.objects.filter(hostel='Mandakini').order_by('room_no')
-    pamba = AvailableRooms.objects.filter(hostel='Pamba').order_by('room_no')
-    sarayu = AvailableRooms.objects.filter(hostel='Sarayu').order_by('room_no')
-    sharav = AvailableRooms.objects.filter(hostel='Sharavati').order_by('room_no')
-    sindhu = AvailableRooms.objects.filter(hostel='Sindhu').order_by('room_no')
-    tambi = AvailableRooms.objects.filter(hostel='Tamraparani').order_by('room_no')
-    sarayu_extn = AvailableRooms.objects.filter(hostel='Sarayu Extn').order_by('room_no')
-    c28 = AvailableRooms.objects.filter(hostel='C-2-8').order_by('room_no')
-    html_content =  render_to_string('controlroom/RoomMap.html', locals(),RequestContext(request))
-    dajax.assign('#tab1',"innerHTML",html_content)
+    hostel_selected = AvailableRooms.objects.filter(hostel=hostel_name).order_by('room_no')
+    html_content =  render_to_string('hospi/RoomMap.html', locals(),RequestContext(request))
+    dajax.remove_css_class=('#id_modal','hide')
+    dajax.assign('#id_modal',"innerHTML",html_content)
     return dajax.json()
