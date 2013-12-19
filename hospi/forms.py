562435c6ea93e8e38aaacc365dd4bc191e4f59be
SourbhBh <eccentricph@gmail.com>
2013-12-20 00:48:10 +0530

    Forgot to add forms

--- hospi/forms.py
+++ hospi/forms.py
@@ -7,4 +7,21 @@ class AddRoomForm(ModelForm):
         model = AvailableRooms
         exclude = ('already_checkedin','mattresses',)
 
+class IndividualForm(ModelForm):
+    class Meta:
+        model = IndividualCheckIn
+        fields = ('room',
+                'duration_of_stay',
+                'number_of_mattresses_given',
+                'mattress_room',
+                'shaastra_ID',
+                'first_name',
+                'last_name',
+                'phone_no',
+                'check_in_control_room',
+                'check_out_control_room',
+                'comments',
+                )
+
+
 
