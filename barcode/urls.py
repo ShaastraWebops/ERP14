3c8a0fdfad654868576d9d5905d7b0ba747e0dfd
gowtham-v <gowtham.vg.7@gmail.com>
2014-01-01 16:51:17 +0530

    url reset temporary

--- barcode/urls.py
+++ barcode/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('barcode.views',
-    url(r'^barcode/$', 'upload_csv', {'type': "barcodeportal"}, name = "barcodeportal"),
-    url(r'^participants/$', 'upload_csv', {'type': "participantsportal"}, name = "participantsportal"),
-    url(r'^add_single_barcode/$','add_single_entry',{'type': "barcodeportal"},name="add_single_barcode"),
-    url(r'^add_single_participant/$','add_single_entry',{'type': "participantsportal"},name="add_single_participant"),
+    url(r'^csv/barcode/$', 'upload_csv', {'type': "barcodeportal"}, name = "barcodeportal"),
+    url(r'^csv/participants/$', 'upload_csv', {'type': "participantsportal"}, name = "participantsportal"),
+    url(r'^csv/add_single_barcode/$','add_single_entry',{'type': "barcodeportal"},name="add_single_barcode"),
+    url(r'^csv/add_single_participant/$','add_single_entry',{'type': "participantsportal"},name="add_single_participant"),
     url(r'^get_details/$','get_details',name = 'get_details')
 )
