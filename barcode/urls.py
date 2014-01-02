a5d069917a53f08a3849dcf4b85d65231a860191
gowtham-v <gowtham.vg.7@gmail.com>
2014-01-02 15:52:46 +0530

    PPM started

--- barcode/urls.py
+++ barcode/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('barcode.views',
     url(r'^add_single_barcode/$','add_single_entry',{'type': "barcodeportal"},name="add_single_barcode"),
     url(r'^add_single_participant/$','add_single_entry',{'type': "participantsportal"},name="add_single_participant"),
     url(r'^get_details/(?P<sh_id>\w+)/$','get_details'),#tried to get shaastra id search query from url
-    url(r'^get_details','get_details',{'sh_id':None})
+    url(r'^get_details','get_details',{'sh_id':None}),
+    url(r'^csv/barcode/PPM/$', 'upload_ppm'),
 )
