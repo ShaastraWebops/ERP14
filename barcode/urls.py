0c5df0341d3928e319dd3940a72cdd0ae0bcdeff
gowtham-v <gowtham.vg.7@gmail.com>
2014-01-04 07:32:26 +0530

    ws fix

--- barcode/urls.py
+++ barcode/urls.py
@@ -18,5 +18,6 @@ urlpatterns = patterns('barcode.views',
     url(r'^detail_entry',direct_to_template,{'template':'barcode/detail_entry.html'},name = 'detail_entry'),
     url(r'^winners/$','hospi_announce'),
     url(r'^event_winners/(?P<event_id>\w+)/$','event_winners'),
+    url(r'^event_ws_winners/(?P<event_id>\w+)/$','event_ws_winners'),
     url(r'^delete_event_winners/(?P<event_id>\w+)/$','delete_event_winners'),
 )
