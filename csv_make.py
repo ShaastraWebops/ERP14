1e3a323208c7134fdc9e2026986864aac0bacff3
Gowtham V <gowtham-v@users.noreply.github.com>
2014-01-05 10:22:47 +0530

    Create csv_make.py

---
+++ csv_make.py
@@ -0,0 +1,9 @@
+import csv
+from barcode.views import *
+b = open('media/barcode.csv','w')
+a = csv.writer(b)
+data = []
+for barcode in Barcode.objects.all():
+    data.append([barcode.barcode,barcode.shaastra_id])
+a.writerows(data)
+b.close()
